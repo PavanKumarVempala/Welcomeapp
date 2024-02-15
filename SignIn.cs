6e7fedb2e308a6842679c7e1ecc0b2ed766b8c5f
Pavan <vpavankumar1999@gmail.com>
2024-02-15 12:48:16 +0530

    final commit

---
+++ SignIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName,string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password required";
+
+            }
+            else
+            {
+                if((userName=="admin")&&(password=="admin")) 
+                {
+                    msg = "Authenticate Pass";
+                }
+                else
+                {
+                    msg = "Authenticate Fail";
+                }
+            }
+
+            return msg;
+        }
+    }
+}
